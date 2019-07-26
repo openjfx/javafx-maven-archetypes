deb230fde043988cdcb9b547fa0b93705fa3dba7
jose.pereda <jose.pereda@gluonhq.com>
2019-07-26 12:40:25 +0200

    Set default size of FXML app

--- javafx-archetype-fxml/src/main/resources/archetype-resources/src/main/java/App.java
+++ javafx-archetype-fxml/src/main/resources/archetype-resources/src/main/java/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
